279abdeaca2a6b3b634b61d5ebd5165cdf5891ba
Ryo Ogawa <ryo.ogawa29@gmail.com>
2022-12-10 19:36:00 +0900

    入力フォームをコンポーネント化

--- src/components/pages/Create.tsx
+++ src/components/pages/Create.tsx
@@ -8,8 +8,6 @@ import {
 import { useLocation, useNavigate } from 'react-router-dom';
 import {
   DEFAULT_THRESHOLD,
-  MAX_CREATOR_NAME_LENGTH,
-  MAX_STAMP_NAME_LENGTH,
   STAMP_COLOR_RED,
   STAMP_IMAGE_SIZE,
 } from '../../constants';
@@ -18,19 +16,9 @@ import { MapState } from '../../types';
 import NavigationButton from '../parts/NavigationButton';
 import CropperModal from '../templates/CropperModal';
 import Header from '../templates/Header';
-import { useForm } from 'react-hook-form';
-import {
-  collection,
-  doc,
-  GeoPoint,
-  serverTimestamp,
-  setDoc,
-} from 'firebase/firestore';
-import { db, storage } from '../../firebase';
-import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
-import { canvasToBlob } from '../../lib/canvasToBlob';
 import { createImage } from '../../lib/createImage';
 import ColorSelector from '../templates/ColorSelector';
+import StampInputForm from '../templates/StampInputForm';
 
 const Create = (props: { setMapState: Dispatch<SetStateAction<MapState>> }) => {
   const { setMapState } = props;
@@ -46,12 +34,6 @@ const Create = (props: { setMapState: Dispatch<SetStateAction<MapState>> }) => {
     }
   }, []);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ mode: 'onChange', criteriaMode: 'all' });
-
   const [imgUrl, setImgUrl] = useState<string>('');
   const [croppedImgUrl, setCroppedImgUrl] = useState<string>('');
   const [srcCtx, setSrcCtx] = useState<CanvasRenderingContext2D>();
@@ -60,61 +42,6 @@ const Create = (props: { setMapState: Dispatch<SetStateAction<MapState>> }) => {
   const [stampThreshold, setStampThreshold] =
     useState<number>(DEFAULT_THRESHOLD);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
-
-  const onSubmit = handleSubmit(async (data) => {
-    const { stampName, creatorName } = data;
-    const { lat, lng } = location.state.mapState.center;
-    const stampsCollectionRef = doc(collection(db, 'stamps'));
-    const fileName = `${stampsCollectionRef.id}.png`;
-    const storageRef = ref(storage, `stamp-images/${fileName}`);
-    const dstCanvas: HTMLCanvasElement = document.getElementById(
-      'dstCanvas'
-    ) as HTMLCanvasElement;
-    const blob = await canvasToBlob(dstCanvas);
-    const uploadTask = uploadBytesResumable(storageRef, blob, {
-      contentType: 'image/png',
-    });
-
-    uploadTask.on(
-      'state_changed',
-      (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-        switch (snapshot.state) {
-          case 'paused':
-            console.log('Upload is paused');
-            break;
-          case 'running':
-            console.log('Upload is running');
-            break;
-        }
-      },
-      (error) => {
-        console.warn(error);
-      },
-      async () => {
-        try {
-          const downloadUrl = await getDownloadURL(uploadTask.snapshot.ref);
-          await setDoc(stampsCollectionRef, {
-            name: stampName,
-            coordinates: new GeoPoint(lat, lng),
-            address: 'テスト県テスト市テスト1-1-1',
-            imageUrl: downloadUrl,
-            createdBy: creatorName,
-            createdAt: serverTimestamp(),
-            stampedCount: 0,
-          });
-          navigate(`/home`, {
-            state: { from: 'CreateLocation' },
-            replace: true,
-          });
-        } catch (error) {
-          console.warn(error);
-        }
-      }
-    );
-  });
 
   useEffect(() => {
     const srcCanvas: HTMLCanvasElement = document.createElement('canvas');
@@ -250,63 +177,10 @@ const Create = (props: { setMapState: Dispatch<SetStateAction<MapState>> }) => {
           }}
         ></input>
         <ColorSelector setStampColor={setStampColor}></ColorSelector>
-        <form onSubmit={onSubmit}>
-          <div>
-            <input
-              {...register('stampImage', {
-                required: true,
-              })}
-              type='file'
-              accept='image/*'
-              onChange={onFileChange}
-            ></input>
-            {errors.stampImage?.type === 'required' && (
-              <div className='text-red-500'>ファイルを選択してください。</div>
-            )}
-          </div>
-          <div>
-            <label>スタンプ名</label>
-            <input
-              {...register('stampName', {
-                required: true,
-                maxLength: MAX_STAMP_NAME_LENGTH,
-              })}
-              className='block px-3 py-1.5 m-0 border border-solid border-gray-300 rounded'
-            />
-            {errors.stampName?.type === 'required' && (
-              <div className='text-red-500'>入力必須の項目です。</div>
-            )}
-            {errors.stampName?.type === 'maxLength' && (
-              <div className='text-red-500'>
-                {MAX_STAMP_NAME_LENGTH}文字以内で入力してください。
-              </div>
-            )}
-          </div>
-          <div>
-            <label>作者</label>
-            <input
-              {...register('creatorName', {
-                required: true,
-                maxLength: MAX_CREATOR_NAME_LENGTH,
-              })}
-              className='block px-3 py-1.5 m-0 border border-solid border-gray-300 rounded'
-            />
-            {errors.creatorName?.type === 'required' && (
-              <div className='text-red-500'>入力必須の項目です。</div>
-            )}
-            {errors.creatorName?.type === 'maxLength' && (
-              <div className='text-red-500'>
-                {MAX_CREATOR_NAME_LENGTH}文字以内で入力してください。
-              </div>
-            )}
-          </div>
-          <button
-            type='submit'
-            className='bg-gray-400 text-white rounded px-2 py-2 font-bold mt-2'
-          >
-            作成する
-          </button>
-        </form>
+        <StampInputForm
+          onFileChange={onFileChange}
+          position={location.state.mapState.center}
+        ></StampInputForm>
       </div>
     </>
   );
---
+++ src/components/templates/StampInputForm.tsx
@@ -0,0 +1,149 @@
+import {
+  collection,
+  doc,
+  GeoPoint,
+  serverTimestamp,
+  setDoc,
+} from 'firebase/firestore';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { LatLng } from 'leaflet';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import {
+  MAX_CREATOR_NAME_LENGTH,
+  MAX_STAMP_NAME_LENGTH,
+} from '../../constants';
+import { db, storage } from '../../firebase';
+import { canvasToBlob } from '../../lib/canvasToBlob';
+
+const StampInputForm = (props: {
+  onFileChange: (e: React.ChangeEvent<HTMLInputElement>) => Promise<void>;
+  position: LatLng;
+}) => {
+  const { onFileChange, position } = props;
+
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: 'onChange', criteriaMode: 'all' });
+
+  const onSubmit = handleSubmit(async (data) => {
+    const { stampName, creatorName } = data;
+    const { lat, lng } = position;
+    const stampsCollectionRef = doc(collection(db, 'stamps'));
+    const fileName = `${stampsCollectionRef.id}.png`;
+    const storageRef = ref(storage, `stamp-images/${fileName}`);
+    const dstCanvas: HTMLCanvasElement = document.getElementById(
+      'dstCanvas'
+    ) as HTMLCanvasElement;
+    const blob = await canvasToBlob(dstCanvas);
+    const uploadTask = uploadBytesResumable(storageRef, blob, {
+      contentType: 'image/png',
+    });
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+        }
+      },
+      (error) => {
+        console.warn(error);
+      },
+      async () => {
+        try {
+          const downloadUrl = await getDownloadURL(uploadTask.snapshot.ref);
+          await setDoc(stampsCollectionRef, {
+            name: stampName,
+            coordinates: new GeoPoint(lat, lng),
+            address: 'テスト県テスト市テスト1-1-1',
+            imageUrl: downloadUrl,
+            createdBy: creatorName,
+            createdAt: serverTimestamp(),
+            stampedCount: 0,
+          });
+          navigate(`/home`, {
+            state: { from: 'CreateLocation' },
+            replace: true,
+          });
+        } catch (error) {
+          console.warn(error);
+        }
+      }
+    );
+  });
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <input
+          {...register('stampImage', {
+            required: true,
+          })}
+          type='file'
+          accept='image/*'
+          onChange={onFileChange}
+        ></input>
+        {errors.stampImage?.type === 'required' && (
+          <div className='text-red-500'>ファイルを選択してください。</div>
+        )}
+      </div>
+      <div>
+        <label>スタンプ名</label>
+        <input
+          {...register('stampName', {
+            required: true,
+            maxLength: MAX_STAMP_NAME_LENGTH,
+          })}
+          className='block px-3 py-1.5 m-0 border border-solid border-gray-300 rounded'
+        />
+        {errors.stampName?.type === 'required' && (
+          <div className='text-red-500'>入力必須の項目です。</div>
+        )}
+        {errors.stampName?.type === 'maxLength' && (
+          <div className='text-red-500'>
+            {MAX_STAMP_NAME_LENGTH}文字以内で入力してください。
+          </div>
+        )}
+      </div>
+      <div>
+        <label>作者</label>
+        <input
+          {...register('creatorName', {
+            required: true,
+            maxLength: MAX_CREATOR_NAME_LENGTH,
+          })}
+          className='block px-3 py-1.5 m-0 border border-solid border-gray-300 rounded'
+        />
+        {errors.creatorName?.type === 'required' && (
+          <div className='text-red-500'>入力必須の項目です。</div>
+        )}
+        {errors.creatorName?.type === 'maxLength' && (
+          <div className='text-red-500'>
+            {MAX_CREATOR_NAME_LENGTH}文字以内で入力してください。
+          </div>
+        )}
+      </div>
+      <button
+        type='submit'
+        className='bg-gray-400 text-white rounded px-2 py-2 font-bold mt-2'
+      >
+        作成する
+      </button>
+    </form>
+  );
+};
+
+export default StampInputForm;
